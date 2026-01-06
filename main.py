566fb53b21d674d5492cf88bdcee1191cb63a718
PASTRAWA <126355129+PASTRAWA@users.noreply.github.com>
2026-01-07 02:09:26 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -2,34 +2,16 @@ import psutil
 import serial
 import time
 import GPUtil
-ser = serial.Serial('/dev/ttyUSB0', 9600)
+ser = serial.Serial('/dev/ttyUSB0', 9600) #Instead of ‘/dev/ttyUSB0’, enter the location where you connected your Arduino. 
 def sender():
     while True:
         gpus=GPUtil.getGPUs()
         for gpu in gpus:
-            #print(gpu.id,gpu.name)
             gpuLoad=int(gpu.load*100)
-            #print(f"Boş Bellek: {gpu.memoryFree}MB")
-            #print(f"Kullanılan Bellek: {gpu.memoryUsed}MB")
-            #print(f"Toplam Bellek: {gpu.memoryTotal}MB")
-            #print(f"Sıcaklık: {gpu.temperature} °C")
-            #print("-" * 30)
         cpu = int(psutil.cpu_percent())
         ram = int(psutil.virtual_memory().percent)
         msg = f"{cpu},{ram},{int(gpuLoad)}\n"
         ser.write(msg.encode())
         time.sleep(1)
-
-def gpuKullanimi(gpu):
-    for gpu in gpus:
-        print(gpu.id,gpu.name)
-        print(f"GPU Yükü: {gpu.load*100}%")
-        print(f"Boş Bellek: {gpu.memoryFree}MB")
-        print(f"Kullanılan Bellek: {gpu.memoryUsed}MB")
-        print(f"Toplam Bellek: {gpu.memoryTotal}MB")
-        print(f"Sıcaklık: {gpu.temperature} °C")
-        print("-" * 30)
-
-
 sender()
 
